d79ee69083b042d01c98e6462a63ff432064f342
Steven <steven.deblois52@gmail.com>
2020-01-24 21:08:32 -0500

    Add Proxy static property on HttpClientFactory

--- src/Http/HttpClientFactory.cs
+++ src/Http/HttpClientFactory.cs
@@ -1,16 +1,18 @@
 ﻿using System;
+using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 
 namespace B2Net.Http {
 	public static class HttpClientFactory {
 	    private static HttpClient _client;
+		public static IWebProxy Proxy { get; set; }
 
         public static HttpClient CreateHttpClient(int timeout) {
             if (_client == null) {
-                var handler = new HttpClientHandler() { AllowAutoRedirect = true };
+				var handler = new HttpClientHandler() { AllowAutoRedirect = true, Proxy = Proxy };
 
-                _client = new HttpClient(handler);
+				_client = new HttpClient(handler, true);
 
                 _client.Timeout = TimeSpan.FromSeconds(timeout);
 
